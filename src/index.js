65a7f94977c48af089fb91256c028130472d1313
neakros <neakros150@gmail.com>
2022-07-28 15:49:45 +0300

    fixWebpack

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 import { example } from '@/js/example'
 
 // Test import of an asset
-import webpackLogo from '@/images/webpack-logo.svg'
+// import webpackLogo from '@/images/webpack-logo.svg'
 
 // Test import of styles
 import '@/styles/index.scss'
 
 // Appending to the DOM
 const logo = document.createElement('img')
-logo.src = webpackLogo
+// logo.src = webpackLogo
 
 const heading = document.createElement('h1')
 heading.textContent = example()
